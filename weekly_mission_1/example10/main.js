963d2d819e2dae18056910c90d6725a79a4c3b92
Saúl Larios <99065353+Saul-Larios@users.noreply.github.com>
2022-04-08 15:34:39 -0500

    Add files via upload

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,18 @@
+import MyPokemon from './pokemon.js'
+
+const pikachu = new MyPokemon('Pikachu')
+const bulbasaur = new MyPokemon("bulbasaur")
+const squirtle = new MyPokemon("squirtle")
+const charmander = new MyPokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
