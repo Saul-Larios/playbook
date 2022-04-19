08cd9fca9141ec15494d6fd6ab5f80c8f79d2d21
Jesus Larios <sau30jas@gmail.com>
2022-04-18 22:28:04 -0500

    agregando ejercicio 2

---
+++ weekly_mission_2/exercises/example_2/exersise_2.js
@@ -0,0 +1,69 @@
+const explorers = [
+    {
+      name: "Saul Larios",
+      exercises_completed: 10,
+      rate: 99,
+      city: "GDL",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Luis Felipe",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Gabo Gabin",
+      exercises_completed: 3,
+      city: "CDMX",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+   ]
+   
+   explorers.forEach(explorers => console.log(explorers.name))
+   explorers.forEach(explorers => console.log(explorers.stack))
+   const newList = explorers.map(function(explorers){ return explorers.stack})
+   console.log(newList)
+   const countries7 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+   const explorersJs = explorers.filter((explorers) =>
+   explorers.stack.includes('js'))
+   console.log(explorersJs)
