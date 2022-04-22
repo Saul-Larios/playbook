7db78a697899454fff07bba8e9b5bd3d6b0c0318
Jesus Larios <sau30jas@gmail.com>
2022-04-21 22:23:30 -0500

    agregando los ultimos ejercicios del example_2

---
+++ weekly_mission_2/exercises/example_2/a.js
@@ -0,0 +1,4 @@
+const bools = [false, false, false, false]
+// Uso de Some para ver si al menos uno de los elementos es false
+const areSomeTrue = bools.some((b) =>  b === true)
+console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //true
